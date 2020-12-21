8ebd447da3ddfd375e3497f51722a3687be14e6f
T3NED <t3ned.root@gmail.com>
2020-12-21 03:50:23 +0000

    ✨ Calculate cluster count & validate shard count

--- src/cluster/ClusterManager.ts
+++ src/cluster/ClusterManager.ts
@@ -1,6 +1,12 @@
 import { Client, ClientOptions } from "eris";
-import { ShardQueue } from "../util/ShardQueue";
+import { Cluster } from "./Cluster";
+
 import { EventEmitter } from "events";
+import { isMaster, setupMaster } from "cluster";
+import { cpus } from "os";
+
+import { ShardQueue } from "../util/ShardQueue";
+import Logger from "../util/Logger";
 
 export class ClusterManager extends EventEmitter {
     public queue = new ShardQueue();
@@ -9,14 +15,30 @@ export class ClusterManager extends EventEmitter {
 
     public token: string;
 
+    public shardCount: number | "auto";
+    public firstShardID: number;
+    public lastShardID: number;
+    public guildsPerShard: number;
+
+    public clusterCount: number | "auto";
+    public shardsPerCluster: number;
+
     public constructor(token: string, options: Partial<ClusterManagerOptions> = {}) {
         super();
 
         // Hide the token when the manager is logged
         Object.defineProperty(this, "token", { value: token });
+        Object.defineProperty(this, "client", { value: new Client(this.token) });
 
         this.clientBase = options.client ?? Client;
-        this.client = new Client(this.token);
+
+        this.shardCount = options.shardCount ?? "auto";
+        this.firstShardID = options.firstShardID ?? 0;
+        this.lastShardID = options.lastShardID ?? 0;
+        this.guildsPerShard = options.guildsPerShard ?? 1000;
+
+        this.clusterCount = options.clusterCount ?? "auto";
+        this.shardsPerCluster = options.shardsPerCluster ?? 0;
 
         this.launchClusters();
     }
@@ -26,8 +48,69 @@ export class ClusterManager extends EventEmitter {
      */
     public launchClusters() {
         // TODO - Calculate shard count
-        // TODO - Calculate cluster count
-        // TODO - Spawn clusters
+        if (isMaster) {
+            // TODO - Print ascii art name
+
+            process.on("uncaughtException", (error) => {
+                Logger.error("Cluster Manager", error.stack ?? error.message);
+            });
+
+            process.nextTick(async () => {
+               Logger.info("Cluster Manager", "Initialising clusters...");
+
+               this.shardCount = await this.validateShardCount();
+               this.clusterCount = this.calculateClusterCount();
+               this.lastShardID ||= this.shardCount - 1;
+
+               Logger.info("Cluster Manager", `Starting ${this.clusterCount} clusters with ${this.shardCount} shards`);
+               setupMaster({ silent: false });
+
+               // TODO - Start clusters
+            });
+        } else {
+            const cluster = new Cluster(this);
+            cluster.spawn();
+        }
+
+        // TODO - Listen for process messages
+    }
+
+    /**
+     * Ensures a valid shardCount is provided
+     * @private
+     */
+    private async validateShardCount() {
+        const { shards } = await this.client.getBotGateway();
+        const { shardCount, guildsPerShard } = this;
+
+        if (typeof shardCount === "number") {
+            if (shardCount < shards)
+                throw new TypeError(`Invalid \`shardCount\` provided. Recommended: ${shards}`);
+            // Provided shardCount is valid
+            return Promise.resolve(shardCount);
+        }
+
+        // Calculate the total shards when shardCount is set to auto
+        const maxPossibleGuildCount = shards * 1000;
+        const shardCountDecimal = maxPossibleGuildCount / guildsPerShard;
+        return Math.ceil(shardCountDecimal);
+    }
+
+    /**
+     * Calculates the total clusters to launch
+     * @private
+     */
+    private calculateClusterCount() {
+        const { clusterCount, shardsPerCluster, shardCount } = this;
+
+        // Provided clusterCount is valid
+        if (typeof clusterCount === "number") return clusterCount;
+        // Use the count of cpus to determine cluster count
+        if (!shardsPerCluster) return cpus().length;
+
+        // Calculate the total clusters with the configured options
+        const clusterCountDecimal = (shardCount as number) / shardsPerCluster;
+        return Math.ceil(clusterCountDecimal);
     }
 }
 
@@ -38,4 +121,12 @@ export interface ClusterManager {
 export interface ClusterManagerOptions {
     client: typeof Client;
     clientOptions: ClientOptions;
+
+    shardCount: number | "auto";
+    firstShardID: number;
+    lastShardID: number;
+    guildsPerShard: number;
+
+    clusterCount: number | "auto";
+    shardsPerCluster: number;
 }
