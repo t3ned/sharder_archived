7c2cff2ea92edffb1885d94f21282d01a15ac9fc
T3NED <t3ned.root@gmail.com>
2020-12-26 23:40:10 +0000

    ✨ Connect all shards to discord

--- src/cluster/Cluster.ts
+++ src/cluster/Cluster.ts
@@ -2,11 +2,15 @@ import { Client } from "eris";
 import { ClusterManager } from "./ClusterManager";
 
 export class Cluster {
-    public clientBase: typeof Client;
     public client: Client;
-    public manager = ClusterManager;
+    public manager: ClusterManager;
 
     public id = -1;
+
+    public shardCount = 0;
+    public maxShards = 0;
+    public firstShardID = 0;
+    public lastShardID = 0;
 
     public constructor(manager: ClusterManager) {
         Object.defineProperty(this, "manager", { value: manager });
@@ -15,11 +19,55 @@ export class Cluster {
     public spawn() {
         // TODO - uncaughtException
         // TODO - unhandledRejection
-        // TODO - connect process message
+
+        process.on("message", message => {
+            if (!message.name) return;
+
+            switch (message.name) {
+                case "connect":
+                    this.firstShardID = message.firstShardID;
+                    this.lastShardID = message.lastShardID;
+                    this.id = message.clusterID;
+                    this.shardCount = message.shardCount;
+                    if (this.shardCount > 0) this.connect();
+                    break;
+            }
+        });
     }
 
     public connect() {
+        const loggerSource = `Cluster ${this.id}`;
+        let { logger, clientOptions, token, clientBase } = this.manager;
 
+        logger.info(loggerSource, `Connecting with ${this.shardCount} shards`);
+
+        // Overwrite passed clientOptions
+        const options = {
+            autoreconnect: true,
+            firstShardID: this.firstShardID,
+            lastShardID: this.lastShardID,
+            maxShards: this.shardCount
+        };
+
+        Object.assign(clientOptions, options);
+
+        // Initialise the client
+        const client = new clientBase(token, clientOptions);
+        this.client = client;
+
+        client.on("connect", (id) => {
+            logger.debug(loggerSource, `Shard ${id} established connection`);
+        });
+
+        client.on("shardReady", (id) => {
+            logger.debug(loggerSource, `Shard ${id} is ready`);
+        });
+
+        client.on("ready", () => {
+           logger.debug(loggerSource, `Shards ${this.firstShardID} - ${this.lastShardID} are ready`)
+        });
+
+        client.connect();
     }
 }
 
