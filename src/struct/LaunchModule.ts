5814233d5cd5acdaa0915cf67818aebe7b9a4c6d
T3NED <software@t3ned.com>
2021-02-24 12:38:57 +0000

    🐛 Fix typings for custom client in LaunchModule

--- src/struct/LaunchModule.ts
+++ src/struct/LaunchModule.ts
@@ -1,12 +1,12 @@
 import type { Client } from "eris";
 import type { IPC } from "./IPC";
 
-export abstract class LaunchModule {
-  public client: Client;
+export abstract class LaunchModule<T extends Client = Client> {
+  public client: T;
   public clusterID: number;
   public ipc: IPC;
 
-  public constructor(client: Client) {
+  public constructor(client: T) {
     Object.defineProperty(this, "client", { value: client });
     this.clusterID = client.cluster.id;
     this.ipc = client.cluster.ipc;
