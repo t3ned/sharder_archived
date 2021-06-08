d8ffb4eecc24a171b1abb41a3b54a4f1da68cc8a
T3NED <software@t3ned.com>
2021-06-08 03:25:09 +0100

    ✨ Add some base strategies

--- src/struct/Strategy.ts
+++ src/struct/Strategy.ts
@@ -1,4 +1,5 @@
-import type { ClusterManager } from "../index";
+import type { ClusterManager, ClusterConfig } from "../index";
+import cluster from "cluster";
 
 export interface IClusterStrategy {
   /**
@@ -13,7 +14,7 @@ export interface IClusterStrategy {
   run(manager: ClusterManager): Promise<void>;
 }
 
-export interface IConnectionStrategy {
+export interface IConnectStrategy {
   /**
    * The name of the cluster strategy.
    */
@@ -23,5 +24,39 @@ export interface IConnectionStrategy {
    * Runs the strategy.
    * @param manager The manager running the strategy
    */
-  run(manager: ClusterManager): Promise<void>;
+  run(manager: ClusterManager, clusterConfigs: ClusterConfig[]): Promise<void>;
+}
+
+/**
+ * The shared cluster strategy will assign all shards to 1 cluster.
+ */
+export function sharedClusterStrategy(): IClusterStrategy {
+  return {
+    name: "shared",
+    run: async (manager) => {
+      const shardCount = await manager.fetchShardCount();
+
+      manager.addCluster({
+        id: 0,
+        firstShardId: 0,
+        lastShardId: shardCount - 1
+      });
+    }
+  };
+}
+
+/**
+ * The ordered connect strategy will connect the clusters in order of id.
+ */
+export function orderedConnectStrategy(): IConnectStrategy {
+  return {
+    name: "ordered",
+    run: async (manager, clusterConfigs) => {
+      const clusters = clusterConfigs.sort((a, b) => b.id - a.id);
+
+      for (let i = 0; i < clusters.length; i++) {
+        // TODO - manager.startCluster(clusters[i])
+      }
+    }
+  };
 }
