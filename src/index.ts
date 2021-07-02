110438a5a968808b9eef7e9057edad97c79e886f
T3NED <software@t3ned.com>
2021-07-02 20:54:41 +0100

    ✨ Update lib exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-export * from "./cluster/ClusterManager";
 export * from "./cluster/Cluster";
+export * from "./cluster/ClusterManager";
 export * from "./struct/LaunchModule";
+export * from "./struct/Logger";
+export * from "./struct/Strategy";
 export * from "./util/Constants";
-export { IPCMessage, Callback } from "./struct/IPC";
+export type { IPC, IPCMessage, IPCMessageOp } from "./ipc/IPC";
+export type { ClusterIPCCallback } from "./ipc/ClusterIPC";
+export type { MasterIPCCallback } from "./ipc/MasterIPC";
