a89666c7f53040d2a6a233e090c535da42474b97
T3NED <software@t3ned.com>
2021-07-02 15:50:18 +0100

    ✨ Export constants

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./cluster/ClusterManager";
 export * from "./cluster/Cluster";
 export * from "./struct/LaunchModule";
+export * from "./util/Constants";
 export { IPCMessage, Callback } from "./struct/IPC";
