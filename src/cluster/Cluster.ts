0ecd5658a30102c499533bde7e2b2399e2226abc
T3NED <software@t3ned.com>
2021-07-02 22:47:35 +0100

    ✨ Add launchmodule and client events to cluster

--- src/cluster/Cluster.ts
+++ src/cluster/Cluster.ts
@@ -1,7 +1,8 @@
-import type { Client, Shard } from "eris";
+import type { Client, ClientOptions, Shard } from "eris";
 import type { ClusterManager } from "./ClusterManager";
 import { LaunchModule } from "../struct/LaunchModule";
 import { ClusterIPC } from "../ipc/ClusterIPC";
+import { join } from "path";
 
 export class Cluster {
   public client?: Client;
@@ -35,18 +36,75 @@ export class Cluster {
    * Initialises the cluster
    */
   public spawn() {
+    const { clientOptions, clientBase, token, logger } = this.manager;
+
     process.on("uncaughtException", (error) => {
-      this.manager.logger.error(`Cluster ${this.id}`, error);
+      logger.error(`Cluster ${this.id}: ${error}`);
     });
 
     process.on("unhandledRejection", (error: Error) => {
-      this.manager.logger.error(`Cluster ${this.id}`, error);
+      logger.error(`Cluster ${this.id}: ${error}`);
     });
 
-    // TODO - initalise client
-    // TODO - update cluster status with client events
+    const options: ClientOptions = {
+      ...clientOptions,
+      autoreconnect: true,
+      firstShardID: this.firstShardID,
+      lastShardID: this.lastShardID,
+      maxShards: 1 // TODO
+    };
+
+    const client = new clientBase(token, options);
+    Reflect.defineProperty(this, "client", { value: client });
+    this.client!.cluster = this;
+
+    const launchModule = this._getLaunchModule();
+    if (launchModule) launchModule.init();
+
     // TODO - add optional synced request handler
-    // TODO - webhooks
+
+    this.status = "WAITING";
+
+    client.on("connect", (id) => {
+      logger.debug(`Cluster ${id} established a connection`);
+    });
+
+    client.on("shardReady", (id) => {
+      if (id === this.firstShardID) this.status = "CONNECTING";
+      logger.debug(`Cluster ${id} is ready`);
+    });
+
+    client.on("ready", () => {
+      this.status = "READY";
+      logger.debug(`Shards ${this.firstShardID} - ${this.lastShardID} are ready`);
+    });
+
+    client.once("ready", () => {
+      this.launchModule?.launch();
+    });
+
+    client.on("shardDisconnect", (_error, id) => {
+      logger.error(`Shard ${id} disconnected`);
+
+      if (this.isDead) {
+        this.status = "DEAD";
+        logger.error(`Cluster ${this.id} died`);
+      }
+    });
+
+    client.on("shardResume", (id) => {
+      if (this.status === "DEAD") this.status = "CONNECTING";
+      logger.warn(`Shard ${id} resumed`);
+    });
+
+    client.on("error", (error, id) => {
+      logger.error(`Shard ${id} error: ${error}`);
+    });
+
+    client.on("warn", (message, id) => {
+      logger.warn(`Shard ${id} warning: ${message}`);
+    });
+
     // TODO - stats
   }
 
@@ -74,9 +132,43 @@ export class Cluster {
   public get uptime() {
     return process.uptime() * 1000;
   }
+
+  /**
+   * Checks whether or not the cluster is dead.
+   * @returns Whether or not the cluster is dead
+   */
+  public get isDead() {
+    return !!this.client?.shards.every((shard) => shard.status === "disconnected");
+  }
+
+  /**
+   * Resolves the launch module.
+   * @returns The launch module
+   */
+  private _getLaunchModule(): LaunchModule | null {
+    const path = join(process.cwd(), this.manager.options.launchModulePath);
+
+    if (!this.manager.options.launchModulePath) return null;
+
+    try {
+      let module = require(path);
+      if (module.default !== undefined) module = module.default;
+      if (module.prototype instanceof LaunchModule) {
+        this.launchModule = <LaunchModule>new module(this.client);
+      } else {
+        this.manager.logger.error("You must inherit the `LaunchModule class.");
+      }
+    } catch (error) {
+      this.manager.logger.error(`Unable to resolve launchModulePath: ${path}`);
+
+      return null;
+    }
+
+    return this.launchModule;
+  }
 }
 
-export type ClusterStatus = "IDLE" | "QUEUED" | "CONNECTING" | "READY" | "DEAD";
+export type ClusterStatus = "IDLE" | "WAITING" | "CONNECTING" | "READY" | "DEAD";
 
 export interface ClusterStats {
   id: number;
