2e86c95636e1a8f29ac1c4515387adf4d437e348
T3NED <software@t3ned.com>
2021-07-04 00:58:35 +0100

    ✨ Export version

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@ export * from "./util/Constants";
 export type { IPC, IPCMessage, IPCMessageOp } from "./ipc/IPC";
 export type { ClusterIPCCallback } from "./ipc/ClusterIPC";
 export type { MasterIPCCallback } from "./ipc/MasterIPC";
+export const VERSION = require("../package.json").version;
